0ea6e6e629b7b48fba3ba50df8814bce1e8a88b9
AndiAlfiansyah <m.andi@41studio.com>
2018-01-05 17:05:46 +0700

    update import

--- backend/app/controllers/spree/admin/import/xlsx/resource_controller.rb
+++ backend/app/controllers/spree/admin/import/xlsx/resource_controller.rb
@@ -3,15 +3,29 @@ module Spree
 		class Import::Xlsx::ResourceController < ApplicationController
 		  skip_before_action :verify_authenticity_token
 
-		  def create
-		    binding.pry
-		    data =  Spree::Admin::Import::Xlsx::Resource.new(params[:file]).extract
-		    render json: model.create(data)
-		  end
+			def create
+				
+			  data =  extract
+			  render json: model.create(data)
+			end
 
-		  def model
-		    @model ||= eval(params[:type])
-		  end
+			def model
+			  @model ||= params[:type]
+			end
+
+			def extract
+				file = params[:import_export][:file]
+			  xlsx  = Roo::Spreadsheet.open(file)
+			  sheet = xlsx.sheet(0) # What an assumption ^_^
+			  attrs = sheet.row(1)
+			  data = sheet.parse(header_search: attrs).map do |d|
+			    d.map {|k, v| [k.parameterize.underscore.to_sym , v] }.to_h.except :id
+			  end
+			end
+
+			private
+			attr_reader :file
+
 		end
 	end
 end
