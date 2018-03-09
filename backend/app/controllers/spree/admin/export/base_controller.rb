57240bdae3d289646e7512ee5a6a0ac515d256d6
Fauzi Rachman <fauzi@41studio.com>
2018-03-09 13:50:31 +0700

    debug export feature

--- backend/app/controllers/spree/admin/export/base_controller.rb
+++ backend/app/controllers/spree/admin/export/base_controller.rb
@@ -4,7 +4,9 @@ module Spree
 		  protected
 
 		  def set_attachment_name(name)
+		  	puts "set_attachment_name 1"
 		    escaped = URI.encode(name)
+		  	puts "set_attachment_name 2"
 		    response.headers['Content-Disposition'] = "attachment; filename*=UTF-8''#{escaped}"
 		  end
 		end
--- backend/app/controllers/spree/admin/export/xlsx/resource_controller.rb
+++ backend/app/controllers/spree/admin/export/xlsx/resource_controller.rb
@@ -5,18 +5,25 @@ module Spree
       before_action :set_resources, only: :index
 
       def index
+        puts "Start"
         respond_to do |format|
+          puts "respond_to 1"
           format.xlsx { set_attachment_name "#{@resource} Data #{Time.now.utc.strftime('%Y%M%d%H%M%S')}.xlsx" }
+          puts "respond_to 2"
         end
       end
 
       private
         def set_resource
+          puts "set_resource 1"
           @resource   = eval(params[:model])
+          puts "set_resource 2"
         end
 
         def set_resources
-          @resources  = @resource.all
+          puts "set_resources 1"
+          @resources  = Spree::Product.all
+          puts "set_resources 2"
         end
     end
   end
