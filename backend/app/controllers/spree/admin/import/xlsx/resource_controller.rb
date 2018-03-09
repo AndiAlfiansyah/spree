b6628b9975c7a33162cba56095971018fc8f6d95
Fauzi Rachman <fauzi@41studio.com>
2018-03-09 14:14:10 +0700

    fix namespace error

--- backend/app/controllers/spree/admin/import/xlsx/resource_controller.rb
+++ backend/app/controllers/spree/admin/import/xlsx/resource_controller.rb
@@ -1,31 +1,33 @@
 module Spree
 	module Admin
-		class Import::Xlsx::ResourceController < ApplicationController
-		  skip_before_action :verify_authenticity_token
+		module Import
+			class Xlsx::ResourceController < ApplicationController
+			  skip_before_action :verify_authenticity_token
 
-			def create
+				def create
 
-			  data =  extract
-			  render json: model.create(data)
+				  data =  extract
+				  render json: model.create(data)
+				end
+
+				def model
+				  @model ||= eval(params[:type])
+				end
+
+				def extract
+					file = params[:import_export][:file]
+				  xlsx  = Roo::Spreadsheet.open(file)
+				  sheet = xlsx.sheet(0) # What an assumption ^_^
+				  attrs = sheet.row(1)
+				  data = sheet.parse(header_search: attrs).map do |d|
+				    d.map {|k, v| [k.parameterize.underscore.to_sym , v] }.to_h.except :id
+				  end
+				end
+
+				private
+				attr_reader :file
+
 			end
-
-			def model
-			  @model ||= eval(params[:type])
-			end
-
-			def extract
-				file = params[:import_export][:file]
-			  xlsx  = Roo::Spreadsheet.open(file)
-			  sheet = xlsx.sheet(0) # What an assumption ^_^
-			  attrs = sheet.row(1)
-			  data = sheet.parse(header_search: attrs).map do |d|
-			    d.map {|k, v| [k.parameterize.underscore.to_sym , v] }.to_h.except :id
-			  end
-			end
-
-			private
-			attr_reader :file
-
 		end
 	end
 end
